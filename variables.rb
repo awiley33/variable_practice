188d25a55458304aaef15fe69541342f6c739ca5
Anna Wiley <awiley33@gmail.com>
2023-03-28 23:40:09 -0500

    Add revisions to file

--- variables.rb
+++ variables.rb
@@ -1,6 +1,15 @@
 username = "awiley33"
+puts username
+username = "soccergirl25"
+puts username
 license_plate = "RLW 3N8"
+puts license_plate
+license_plate = "J8D MW2"
+puts license_plate
 greeting = "Hello World!"
+puts greeting
+greeting = "Goodbye World!"
+puts greeting
 age = 30
 address_number = 1860
 golf_score = -3
@@ -10,9 +19,6 @@ price = 19.99
 balance_paid = false
 assignment_completed = true
 files_up_to_date = true
-username = "soccergirl25"
-license_plate = "J8D MW2"
-greeting = "Goodbye World!"
 puts "It is beautiful outside today!"
 puts "I can't wait to go on a walk"
 puts "My desk setup is coming together."
